d43b73cdc680be6a729b512d8df9354c50df4152
wmax641 <no-reply@wmax641.website>
2024-01-16 20:12:29 +0900

    hackhack

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "base_name" {
   description = "Common prefix used for naming resources of this project"
-  default = "wap-iam-accounts"
+  default     = "wap-iam-accounts"
 }
 variable "common_tags" {
   description = "Common tags used in resources of this project"
