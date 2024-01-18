f6b9e16c1ddc3c33a88c8ba9dd847a419ad6d7b4
wmax641 <no-reply@wmax641.website>
2024-01-18 19:07:18 +0900

    test deploy role

--- role_dummy.tf
+++ role_dummy.tf
@@ -12,6 +12,24 @@ module "test_role" {
     data.aws_iam_policy_document.inline_policy_0.json,
     data.aws_iam_policy_document.inline_policy_1.json,
   ]
+  used_by_repo       = "wmax641/wap-iam-test"
+  tags               = { "additional" = "tag" }
+}
+
+module "test_deploy_role" {
+  source = "./modules/wap-role"
+
+  name = "WAPTestDeployRole4"
+
+  assume_role_policy = data.aws_iam_policy_document.test_role_assume_policy.json
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess",
+    "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess",
+  ]
+  inline_policies = [
+    data.aws_iam_policy_document.inline_policy_0.json,
+    data.aws_iam_policy_document.inline_policy_1.json,
+  ]
   create_deploy_role = true
   used_by_repo       = "wmax641/wap-iam-test"
   tags               = { "additional" = "tag" }
